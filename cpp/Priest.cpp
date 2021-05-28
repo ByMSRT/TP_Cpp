4c2ee5ce96dd168acba5780d9f1ee06d7d7b1ad7
ByMSRT <73824665+ByMSRT@users.noreply.github.com>
2021-05-28 21:35:20 +0200

    Add Priest

---
+++ cpp/Priest.cpp
@@ -0,0 +1,5 @@
+#include "../hpp/Priest.hpp"
+
+Priest::Priest(string name) : Character(name,Job::PriestJob,200,0,15,900) {
+    
+}
---
+++ hpp/Priest.hpp
@@ -0,0 +1,18 @@
+#ifndef PRIEST.HPP
+#define PRIEST.HPP
+#include "./Character.hpp"
+#include <string>
+#include "./Exceptions.hpp"
+
+using namespace std;
+
+class Priest : public Character{
+    private:
+
+
+    public:
+    Priest(string name);
+
+};
+
+#endif
