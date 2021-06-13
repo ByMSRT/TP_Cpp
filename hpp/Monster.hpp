b39e813f77de0546129a94fc5790458ff17c2d27
nicolas <nicolas.segura@ynov.com>
2021-06-13 23:16:42 +0200

    cpp/Priest.cpp

--- hpp/Monster.hpp
+++ hpp/Monster.hpp
@@ -9,16 +9,20 @@ using namespace std;
 
 class Monster : public Character {
     private:
-    vector<Character&> enemies;
-    vector<Character&> allies;
+    vector<Character*> allies;
+    vector<Character*> monsters;
     int selectAction;
     void simpleAtt();
     void groupAtt();
     void upDef();
 
     public:
-    Monster(string name) : Character(name,Job::MonsterJob,200,100,50,1000){}
-    void action(vector<Character&> enemies, vector<Character&> allies);
+    Monster(string name) : Character(name,Job::MonsterJob,200,100,40,1000){}
+
+    void playRound(vector<Character*>& characters, int nbrAllies) override;
+
+    string getSpecialActionName() override;
+    void action(vector<Character*> monsters, vector<Character*> allies);
 };
 
 
