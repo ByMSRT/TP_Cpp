ead9501e02875834daa687877d4abbcd3c0abd6b
nicolas <nicolas.segura@ynov.com>
2021-06-13 23:20:39 +0200

    cpp/Priest.cpp

--- cpp/Monster.cpp
+++ cpp/Monster.cpp
@@ -1,27 +1,29 @@
 #include "../hpp/Monster.hpp"
 
 void Monster::simpleAtt(){
-    Character& randomEnemy = enemies[rand() % enemies.size()];
-    this->attack(randomEnemy);
+    Character* randomAlly = allies[rand() % allies.size()];
+    this->attack(*randomAlly);
 }
 
 void Monster::groupAtt(){
+    cout << "Zone attack : ";
     int tampon = this->physicalAttack;
-    this->physicalAttack = this->physicalAttack / enemies.size();
-    for(Character& enemy : enemies){
-        this->attack(enemy);
+    this->physicalAttack = this->physicalAttack / allies.size();
+    for(Character* ally : allies){
+        this->attack(*ally);
     }
     this->physicalAttack = tampon;
 }
 
 void Monster::upDef(){
-    Character& randomAlly = allies[rand() % allies.size()];
-    randomAlly.shield(20);
+    Character* randomMonster = monsters[rand() % monsters.size()];
+    randomMonster->shield(20);
+    cout << this->toString() << " enhance " << randomMonster->toString() << "'s defense." << endl;
 }
 
-void Monster::action(vector<Character&> enemies, vector<Character&> allies){
-    this->enemies = enemies;
+void Monster::action(vector<Character*> allies, vector<Character*> monsters){
     this->allies = allies;
+    this->monsters = monsters;
 
     selectAction = rand() % 3;
 
@@ -33,4 +35,22 @@ void Monster::action(vector<Character&> enemies, vector<Character&> allies){
         upDef();
     }
     
+}
+
+void Monster::playRound(vector<Character*>& characters, int nbrAllies) {
+    vector<Character*> allies(characters.begin(), characters.begin() + nbrAllies + 1);
+    vector<Character*> monsters(characters.begin() + nbrAllies + 1, characters.end());
+    // cout << "allies : ";
+    // for (Character* character : allies) {
+    //     cout << character->name << endl;
+    // }
+    // cout << "monsters : ";
+    // for (Character* character : monsters) {
+    //     cout << character->name << endl;
+    // }
+    this->action(allies, monsters);
+}
+
+string Monster::getSpecialActionName() {
+    return "Monster attack or enhance other monster defense";
 }
